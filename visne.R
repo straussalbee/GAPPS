95b439c8f1b40eceb1e9a7772bbcd929b53a479a
CLAIRE LEVY <clairel@uw.edu>
2015-08-24 10:49:12 -0700

    updated the code to only include the flowCore methods that actually worked, added more notes and explanations

--- visne.R
+++ visne.R
@@ -10,72 +10,75 @@ library(stringr)
 #locate the folder where the fcs files are 
 folderPath<-"J:\\MacLabUsers\\Claire\\Projects\\GAPPS Project\\GAPPS 2015 Neutrophils\\data and analysis\\fcs and compiled flowjo\\normed live export"
 
+#use this later to name the flow frames
+files<-list.files(folderPath)
+fileNamesOnly<-as.list(str_replace(files,pattern=".fcs",replacement=""))
+
+
 #The flowCore method for reading in files
 frames<-lapply(dir(folderPath, pattern = "\\.fcs",full.names = TRUE),
                read.FCS)
 
 
-as(frames,"flowSet")
+as(frames,"flowSet") #check
 
-names(frames) <- sapply(frames, keyword, "FILENAME")
+#name the list of frames with the lists of file names(minus the .fcs)
+names(frames) <- fileNamesOnly
 
-fs<-as(frames,"flowSet")
+fs<-as(frames,"flowSet") #now I have a flowSet of multiple flowFrames
 
-#a faster way to do it
-fs2<-read.flowSet(dir(folder, pattern = "\\.fcs"),
-                  name.keyword="SAMPLE ID",
-                  phenoData=list(name="SAMPLE ID",Filename="$FIL"))
+
+#extract the data matrix from the flowSet to check if it looks right
+matrix<-fsApply(fs,exprs)
+
+#the first and last 6 rows of this is the same matrix as
+#if you read in the files just as a LIST from the folder 
+#(lappy(files,read.FCS)) and then do lapply(data,exprs)
+# instead of using the flowSet methods so I think both methods
+#get you the same result, i.e. a single data matrix that includes
+#data from all the samples together, in the order that they
+# were read into the flowSet.
+
+#Note: if one wants to compare the matrix you get from reading in
+#the files in different ways, you need to do it before the random
+#subsampling. But maybe if you do set.seed before the random sampling
+#it will end up the same? I haven't tried this yet 24Aug15
+
+
+
 
 ###### SUBSAMPLE THE SAMPLES #####################
 
 cellsPerSample <- 500
 
-# get random sample of cells from each FCS file
+# here is a function to get random sample of cells from each FCS file
 
-MICC_15_130_01_sub <- MICC_15_130_01[sample(1:nrow(MICC_15_130_01), cellsPerSample), ]
-#HVTN5217_sub <- HVTN5217[sample(1:nrow(HVTN5217), cellsPerSample), ]
-HVTN4396_sub <- HVTN4396[sample(1:nrow(HVTN4396), cellsPerSample), ]
-HVTN3633_sub <- HVTN3633[sample(1:nrow(HVTN3633), cellsPerSample), ]
+subSample<-function(Frame){
+  Frame[sample(1:nrow(Frame),cellsPerSample),]
+}
 
-######################  GET CHANNEL NAMES ########################
-#extract the names assigned to the channels from the flowframe(CD3)
-#they are the same for all samples so I can get them from any of the 
-#flow frames
+#run the function on the flowSet
+sampledFs<-fsApply(fs,FUN=subSample)
 
-pData<-pData(parameters(HVTN3633_sub))
 
-#select just the "name" (metal) and the "desc" (the ab assigned to it)
-channelAndDesc<-select(pData,name,desc)
-#changing to characters instead of factos
-channelAndDesc$name<-as.character(channelAndDesc$name)
-channelAndDesc$desc<-as.character(channelAndDesc$desc)
-
-#get rid of the rownames (which indicate keywords??)
-rownames(channelAndDesc)<-NULL
 
 
 ########################### COMBINE FILES ###########################
-#I want to remove any columns that aren't relevant for visualization
-#these are different for sample 5217 since I didn't collect beads
-#For the other samples:
-# skip columns 1,2,6,9,10,13,51,52 (time,event length,beads1, beads2,
-# beadDist and Event #)
-#Ce140 is beads1 amd Ce142 is beads2
-#qdot4 = Cd113 qdot6=Cd116
 
+# arcsinh transformation with cofactor of 5 (divide data by 5)
 
-# @exprs removes matrix from flowframe
+#here is a function to get the actual data matrix out of the subsampled
+#flowframe
+# exprs() removes matrix from flowframe
 #one row per cell, columns are channels
-#rbind multiple samples
+getMatrix<-function(subSampledSet){
+  exprs(subSampledSet)
+}
 
-toCluster <- rbind(MICC_15_130_01_sub@exprs[,-c(1,2,6,9,10,13,51,52)],
-                   #HVTN5217_sub@exprs[,-c(1,2,6,9,51)],
-                   HVTN4396_sub@exprs[,-c(1,2,6,9,10,13,51,52)],
-                   HVTN3633_sub@exprs[ , -c(1,2,6,9,10,13,51,52)])
-# arcsinh transformation with cofactr of 5 (divide data by 5)
+# apply the function to the subSampled flowset
+ toCluster<-fsApply(sampledFs,FUN=getMatrix)
 
-
-toCluster2<-asinh(toCluster/5)# this is what the nolan lab does
+toCluster<-asinh(toCluster/5)# this is what the nolan lab does
 #and probably viSNE because their default is cofactor of 5
 #"xxxx2" going forward comes from the code above
 
@@ -97,52 +100,83 @@ set.seed(123) # always get same result with same data
 #Rtsne gives a LIST of 7 attributes and their values
 #theta, perplexity, N, origD,Y, costs and itercosts
 
-cluster2<-Rtsne(toCluster2, initial_dims = 10^6, max_iter = 500, PCA = FALSE, 
+#This next step takes a minute to run...
+
+cluster<-Rtsne(toCluster, initial_dims = 10^6, max_iter = 500, PCA = FALSE, 
                 perplexity = 30, theta = 0)
 
 
 # tsne1 and tsne2 values are in the matrix in cluster$Y
+#rename them for clarity
 
-colnames(cluster2$Y) <- c("tsne1", "tsne2")
+colnames(cluster$Y) <- c("tsne1", "tsne2")
 
 
-#combine tsne dimensions from the Rtsne functio
+#combine tsne dimensions from the Rtsne function
 #with the mass cytometry data extracted and transformed from the .fcs files
 
-toPlot2 <- cbind(toCluster2, cluster2$Y)
-toPlot2 <- as.data.frame(toPlot2)
+toPlot <- cbind(toCluster, cluster$Y)
+toPlot <- as.data.frame(toPlot)
 
+#toPlot is a df with one column per metal and one row per sample
 
-# label samples: make a column in the toPlot2 df called
+# label samples: make a column in the toPlot df called
 #Sample and repeat the sample name for the number cells sampled
 
-toPlot2$Sample <- c(rep("MICC_15_130_01", cellsPerSample),
-                   #rep("HVTN5217", cellsPerSample),
+toPlot$Sample <- c(rep("MICC_15_130_01", cellsPerSample),
+                   rep("HVTN5217", cellsPerSample),
                    rep("HVTN4396", cellsPerSample),
                    rep("HVTN3633", cellsPerSample))
-
-#toPlot2 is a df with one column per metal and one row per sample
-
-head(toPlot2)
+head(toPlot)
 
 #shows each sample as a different color
-tSNE3samples<-ggplot(as.data.frame(toPlot2), aes(tsne1, tsne2)) +
+tSNE4samples<-ggplot(as.data.frame(toPlot), aes(tsne1, tsne2)) +
   geom_point(aes(color = Sample)) 
-ggsave("tSNE3samples.png",dpi=600)
+ggsave("tSNE4samples10Aug15.png",dpi=600)
 
+
+######################  GET CHANNEL NAMES and MELT  ########################
+#I am doing this so I can make gradient plots of tsne1 by tsne2 for
+#each Ab and have the plot labeled with the Ab instead of with the
+#name of the metal is is conjugated to.
+
+#extract the names assigned to the channels from the flowframe(ex. CD3)
+#they are the same for all samples so I can get them from any of the 
+#flow frames
+
+pData<-as.data.frame(pData(parameters(frames[[1]])))
+
+#select just the "name" (metal) and the "desc" (the ab assigned to it)
+channelAndDesc<-select(pData,name,desc)
+
+#changing to characters instead of factors
+channelAndDesc$name<-as.character(channelAndDesc$name)
+
+#fix the places where there is no entry for desc
+#i.e. channel name is Event_length and Time
+
+channelAndDesc$desc[channelAndDesc$name=="Event_length"]<-"Event Length"
+channelAndDesc$desc[channelAndDesc$name=="Time"]<-"Time"
+
+channelAndDesc$desc<-as.character(channelAndDesc$desc)
+
+#get rid of the rownames (which indicate keywords??)
+rownames(channelAndDesc)<-NULL
+
+######## MELT #####################################
 #now melt the df so you can plot tsne1 vs tsne2 (like in viSNE)and
-#color the plots by the value given for 
-meltToPlot2<-melt(toPlot2,id.vars=c("tsne1","tsne2","Sample"))
 
+meltToPlot<-melt(toPlot,id.vars=c("tsne1","tsne2","Sample"))
 
+###################### MERGE IN NAMES #######################
 #merge in the df of the channels and their corresponding names
+#so they Ab names can be used in the plot
 
-meltToPlot2<-merge(meltToPlot2, channelAndDesc,by.x="variable",by.y="name")
+meltToPlot<-merge(meltToPlot, channelAndDesc,by.x="variable",by.y="name")
 
-allParams<-ggplot(meltToPlot2, aes(x=tsne1,y=tsne2))+
+allParams<-ggplot(meltToPlot, aes(x=tsne1,y=tsne2))+
   geom_point(aes(color = value),alpha=0.6, size=.6) +
-  scale_colour_gradient2("value", low="white",
-                         mid="blue",high="red",mid)+
+  scale_colour_gradientn("value",colours=topo.colors(7))+
   facet_wrap(~desc) +
   xlab("bhSNE1") + 
   ylab("bhSNE2") +
@@ -150,6 +184,6 @@ allParams<-ggplot(meltToPlot2, aes(x=tsne1,y=tsne2))+
     #scale_y_continuous(limits=c(-40,40)) +
   theme(panel.grid.major=element_blank())
 
-ggsave("allParams.png",dpi=600)
+ggsave("allParams10Aug15.png",dpi=600)
                            
 #for scale colour gradientn("value, colours=topo.colors(7))
